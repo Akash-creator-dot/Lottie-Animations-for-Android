a1ff3ff6db6ebee4f9a9868a8cae7e7c1b9c5e53
Akash-creator-dot <akashsolanki926@gmail.com>
2024-07-17 06:00:45 +0530

    lottie

---
+++ app/src/main/java/com/techgiants/lottiecustom/MainActivity.java
@@ -0,0 +1,24 @@
+package com.techgiants.lottiecustom;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class MainActivity extends AppCompatActivity {
+  LottieAnimationView num;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        num=findViewById(R.id.n);
+num.setAnimation(R.raw.numbers);
+num.playAnimation();
+num.loop(true);
+    }
+}
